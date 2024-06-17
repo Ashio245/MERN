c6921b4c5939ea7da1c49bc854908b2006da5d02
John Emerson Delos Reyes <delosreyesjohnemerson@gmail.com>
2024-06-17 17:02:04 +0800

    fixing bugs

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-import http from `http`
+import http from 'http'
+
+http.createServer((req, res)=>{
+
+res.writeHead(400,  {"Content-Type": "text/html"})
+
+res.write('req.url')
+
+
+if (req.method = "POST"){
+    res.write('post request')
+}
+
+res.end()
+
+}).listen(4000, 'localhost')
